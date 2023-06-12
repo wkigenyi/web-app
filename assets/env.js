926555edb186edb5375e20a43b30167a6019e12a
wkigenyi <wkigenyi@gmail.com>
2023-06-12 06:04:48 +0000

    Deploying to gh-pages from  @ 1b60a8f43c7b4892230ed324e164c1b399a199fa 🚀

---
+++ assets/env.js
@@ -0,0 +1,19 @@
+(function(window) {
+  window["env"] = window["env"] || {};
+
+  // BackEnd Environment variables
+  window["env"]["fineractApiUrls"] = '';
+  window["env"]["fineractApiUrl"]  = '';
+
+  window["env"]["apiProvider"] = '/fineract-provider/api';
+  window["env"]["apiVersion"]  = '/v1';
+
+  window["env"]["fineractPlatformTenantId"]  = '';
+  window["env"]["fineractPlatformTenantIds"]  = '';
+
+  // Language Environment variables
+  window["env"]["defaultLanguage"] = '';
+  window["env"]["supportedLanguages"] = '';
+
+  window['env']['preloadClients'] = '';
+})(this);
