a1deb4a7dcdd321edbe3d665a719ea21d9433089
Wilfred Kigenyi <wkigenyi@gmail.com>
2024-10-11 12:26:22 +0300

    use rosema branch

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -29,7 +29,7 @@ export const environment = {
   },
   warningDialog: {
     title: 'Warning',
-    content: 'This system is for authorized use only. Unauthorized access will result in possible legal action. By accessing this system, you acknowledge that you are authorized to do so and that all data stored and processed here is confidential.',
+    content: 'This system is for AUTHORISED USE ONLY. Unauthorized access will result in possible legal action. By accessing this system, you acknowledge that you are authorized to do so and that all data stored and processed here is confidential.',
     buttonText: 'Close'
   },
   defaultLanguage: window['env']['defaultLanguage'] || 'en-US',
